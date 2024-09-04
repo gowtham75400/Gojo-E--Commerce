a7b558da39027d7192e665c34964609ae27a1802
gowthamnk <gowthamnk67@gmail.com>
2024-09-04 11:42:35 +0530

    new commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "frontend", // Set the root directory to 'frontend'
   plugins: [react()],
   server: {
     proxy: {
       "/api/": "http://localhost:5000",
       "/uploads/": "http://localhost:5000",
+    },
+  },
+  build: {
+    outDir: "../dist", // Specify the output directory for the build
+    rollupOptions: {
+      input: "frontend/index.html", // Ensure that the entry point is the correct path to index.html
     },
   },
 });
